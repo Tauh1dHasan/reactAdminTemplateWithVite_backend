571d96f3e5b1d8e079ec240360dbeacc4246bd3b
tauhid.hasan <tauhid.hasan@sebpo.com>
2024-02-27 18:58:58 +0600

    Working on backend API

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Task;
+
 class TaskController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return response()->json(Task::all());
+    }
 }
